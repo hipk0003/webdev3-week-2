99e1de65ef384f7a5b7c5ac0cdbf398320d5762b
Chris Hipkiss <hipk0003@algonquinlive.com>
2023-01-19 11:13:23 -0500

    working with js

---
+++ main.js
@@ -0,0 +1,50 @@
+console.log("Hello")
+
+const firstName ="Chris"
+
+
+
+const secondName = "William"
+
+console.log(firstName + " " + secondName + " " + "Hipkiss")
+const description = `My name is: ${firstName}
+My middle name: ${secondName}. ${2+2}`
+
+console.log(description)
+
+const age = 9999_99_99
+
+console.log(age)
+
+let isAdmin = true
+//...
+isAdmin = false
+
+const list = [
+    "John"
+]
+
+console.log(list)
+
+const students = [
+    {
+        firstName:"John",
+        lastName:"Connor",
+        age: 33,
+        isAdmine: true
+    },
+    {
+        firstName:"Jill 1",
+        age: 23,
+        isAdmine: true
+    },
+]
+
+console.log (students[1].lastName)
+
+let points = 6
+//..
+points = points + 5;
+points -= 20
+console.log( students [1].lastName == "Connor" || students [1].firstName == "John")
+
